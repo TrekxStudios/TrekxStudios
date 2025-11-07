9f874c657bf38ab2c762e3caef2bfc74e13bd035
Ryan Chamberlin <rychmbr94@gmail.com>
2025-11-07 15:30:57 -0800

    Changed alot of the site.. added canva flier, made carousel only one image and click to enter.. linked my form in the Book now section of the link

---
+++ js/stripe.js
@@ -0,0 +1,18 @@
+// Example of creating a Checkout session for a deposit
+const session = await stripe.checkout.sessions.create({
+  payment_method_types: ['card'],
+  line_items: [{
+    price_data: {
+      currency: 'usd',
+      product_data: {
+        name: 'Deposit for Service',
+      },
+      unit_amount: 5000, // $50.00
+    },
+    quantity: 1,
+  }],
+  mode: 'payment',
+  success_url: 'https://yoursite.com/success',
+  cancel_url: 'https://yoursite.com/cancel',
+});
+
